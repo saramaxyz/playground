89062b8ea81efa9e0e67b28fb43b6b5037e0e2e1
Oguz Vuruskaner <ovuruska@outlook.com>
2022-05-17 11:41:01 +0300

    Convert body type to form data.

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,9 @@
 import React from "react"
 import {Provider} from "react-redux"
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import getStore from "./getStore"
 import {PersistGate} from 'redux-persist/integration/react'
 
-let Router = BrowserRouter
 
 const {store,persistor} = getStore()
 
